c64a01813ad29ca90573a6a13c4bdd7f7cb7b7f0
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-09-26 21:02:42 +0530

    minor-changes

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
-    URI: '',
-    SECRET: '',
-    GITHUB_CLIENT_ID: '', // add client id here
-    GITHUB_CLIENT_SECRET: '', //add client secret here
-    GITHUB_CALLBACK_URL: '',
-    GOOGLE_CLIENT_ID: '',
-    GOOGLE_CLIENT_SECRET: '',
+    URI: 'mongodb+srv://minerva:minerva@cluster0.viwjo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SECRET: 'addingsecretkeyisfun',
+    GITHUB_CLIENT_ID: 'c67f11efcf615018a011', // add client id here
+    GITHUB_CLIENT_SECRET: '89eb42b81454975e357ddcb3bdece542c06bba45', //add client secret here
+    GITHUB_CALLBACK_URL: 'http://localhost:5000/api/auth/github/callback',
+    GOOGLE_CLIENT_ID:  '292921574719-88ujnbthccev1ui8mbkg3lm6dma9aprv.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: 'MqD09jkI8Z3KmoacnOKMYFOP',
 }
