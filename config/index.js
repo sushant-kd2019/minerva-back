4b6e4e7869b362230e36a1bc7c3f00c868512f51
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-09-26 22:45:20 +0530

    debugged

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    console.log("HEEE")
+    
     module.exports = require('./prod')
+    console.log(process.env.SECRET)
 } else {
     
     module.exports = require('./dev')
