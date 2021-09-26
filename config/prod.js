6f48b53696c3db61c65ff38b33fa6fc344781239
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-09-26 22:08:49 +0530

    deploy-bug debugged

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     SECRET: process.env.SECRET,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL,
+    GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL, 
 }
